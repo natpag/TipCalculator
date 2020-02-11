1039522c500c367ca0771de148f8864d8145892d
npagan <npagan@proforma.com>
2020-02-11 11:01:33 -0500

    Added Service level for tip

--- Program.cs
+++ Program.cs
@@ -11,15 +11,35 @@ namespace TipCalculator
       // Ask how much the bill
       Console.WriteLine("How much was your meal?");
       var total = Console.ReadLine();
-      //calculate 18%
-      var tip = double.Parse(total) * 0.18;
+      //how was the service?
+      Console.WriteLine("How was your service?");
+      var service = Console.ReadLine();
+      //okay,good,great, default
+
+      //calculate tip based on the level of service
+      var tipPercentage = 0.15;
+      if (service == "okay")
+      {
+        tipPercentage = 0.18;
+      }
+      else if (service == "good")
+      {
+        tipPercentage = 0.2;
+      }
+      else if (service == "great")
+      {
+        tipPercentage = 0.25;
+      }
+
+      var tip = double.Parse(total) * tipPercentage;
       //add tip to total
       var grandTotal = tip + double.Parse(total);
-      // display the result
+      //add the tip to the total
+      //display the result
+      //tipPercentage * 100 will give the correct value
+      var tipToDisplay = tipPercentage * 100;
       Console.WriteLine("Your total was " + total);
-
-      Console.WriteLine("The 18% tip is " + tip);
-
+      Console.WriteLine("The " + tipToDisplay + "% tip is " + tip);
       Console.WriteLine("The Grand Total is " + grandTotal);
     }
   }
